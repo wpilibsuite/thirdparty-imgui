2502729ac5d1880e9bc6595b9b87c4e7e478ff4d
Peter Johnson <johnson.peter@gmail.com>
2019-10-13 19:53:06 -0700

    Put in absolute path to VS 2019

--- build_fonts.bat
+++ build_fonts.bat
@@ -1,4 +1,4 @@
-call "%vs160comntools%\..\..\VC\Auxiliary\Build\vcvars32.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars32.bat"
 cl /Fe"imgui_font_bin2c" imgui_font_bin2c imgui\misc\fonts\binary_to_compressed_c.cpp
 .\imgui_font_bin2c "proggyfonts/ProggyDotted/ProggyDotted Regular.ttf" ProggyDotted > fonts\ProggyDotted.inc
 
