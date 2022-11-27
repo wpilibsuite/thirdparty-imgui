6e764fbb2b972980975c9a5cafb1f4b0d9ba2789
Peter Johnson <johnson.peter@gmail.com>
2022-11-26 21:54:06 -0800

    Use thirdparty-fonts releases instead of submodules

--- fonts/imgui_FontAwesomeRegular.cpp
+++
@@ -1,5 +0,0 @@
-#include "imgui_FontAwesomeRegular.h"
-#include "FontAwesomeRegular.inc"
-ImFont* ImGui::AddFontAwesomeRegular(ImGuiIO& io, float size_pixels, const ImFontConfig* font_cfg, const ImWchar* glyph_ranges) {
-  return io.Fonts->AddFontFromMemoryCompressedTTF(FontAwesomeRegular_compressed_data, FontAwesomeRegular_compressed_size, size_pixels, font_cfg, glyph_ranges);
-}
--- fonts/imgui_FontAwesomeSolid.cpp
+++
@@ -1,5 +0,0 @@
-#include "imgui_FontAwesomeSolid.h"
-#include "FontAwesomeSolid.inc"
-ImFont* ImGui::AddFontAwesomeSolid(ImGuiIO& io, float size_pixels, const ImFontConfig* font_cfg, const ImWchar* glyph_ranges) {
-  return io.Fonts->AddFontFromMemoryCompressedTTF(FontAwesomeSolid_compressed_data, FontAwesomeSolid_compressed_size, size_pixels, font_cfg, glyph_ranges);
-}
